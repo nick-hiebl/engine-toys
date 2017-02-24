d0fbfb5b62ad28656a574472c1e9e68953ef1b1a
Nick <nicholas@hiebl.net.au>
2017-02-24 19:53:35 +1100

    Added Matrix4, a non-object-oriented approach in far fewer lines

---
+++ Matrix4/my_game.js
@@ -0,0 +1,41 @@
+var streams = [];
+var fSize = 32;
+function char() {
+    return String.fromCharCode(floor(random(0x30A1, 0x30E3)));
+}
+function setup() {
+    fontSize(fSize);
+    resize(window.innerWidth, window.innerHeight);
+    for (var x = 0; x < width; x += fSize) {
+        var s = [], n = floor(random(10, 20)), sp = random(3, 12);
+        for (var i = 0; i < n; i ++) {
+            s.push({
+                char: char(), y: -i * fSize, speed: sp, loopSpeed: random(1/50,1/10), loop: 0
+            });
+        }
+        streams.push(s);
+    }
+}
+function update() {
+    streams.forEach(function(stream) {
+        stream.forEach(function(s) {
+            s.y = s.y > height + 30 ? -30 : s.speed + s.y;
+            s.loop = s.loop > 1 ? 0 : s.loopSpeed;
+            if (s.loop > 1) {
+                s.char = char();
+            }
+        });
+    });
+}
+function draw() {
+    background(0);
+    fill(0, 255, 70);
+    fontSize(fSize/1.25);
+    ctx.shadowBlur = fSize/2;
+    ctx.shadowColor = "#0f7";
+    for (var i = 0; i < streams.length; i ++) {
+        for (var s of streams[i]) {
+            centerText(s.char, i * fSize + fSize/2, s.y);
+        }
+    }
+}
