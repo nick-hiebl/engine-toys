c0de74c9616325c31d7ee70787dbcb0c64736c3e
Nick <nicholas@hiebl.net.au>
2017-03-11 11:34:18 +1100

    Added vector projection example

---
+++ Steering/VectorProjection/my_game.js
@@ -0,0 +1,37 @@
+function dot(v1, v2) {
+    return v1.x * v2.x + v1.y * v2.y;
+}
+
+var a;
+var b;
+
+function setup() {
+    resize(window.innerWidth, window.innerHeight);
+    a = new Vector(random(0.1, 0.4) * width, random(0.2, 0.7) * height);
+    b = new Vector(random(0.6, 0.9) * width, random(0.3, 0.8) * height);
+}
+
+function draw() {
+    background(255);
+    fill(0);
+    stroke(0);
+    lineWidth(3)
+    ellipse(a.x, a.y, 5, 5);
+    ellipse(b.x, b.y, 5, 5);
+    line(a.x, a.y, b.x, b.y);
+    ellipse(MOUSE.x, MOUSE.y, 5, 5);
+    line(a.x, a.y, MOUSE.x, MOUSE.y);
+
+    var ba = b.copy().sub(a);
+    var ma = new Vector(MOUSE.x, MOUSE.y).sub(a);
+
+    var dotProduct = dot(ba, ma);
+    dotProduct /= ba.magnitude;
+    //dotProduct /= ma.magnitude;
+    console.log(dotProduct);
+
+    var c = ba.normalise(dotProduct).add(a);
+    line(c.x, c.y, MOUSE.x, MOUSE.y);
+    fill(255, 0, 0);
+    ellipse(c.x, c.y, 5, 5);
+}
