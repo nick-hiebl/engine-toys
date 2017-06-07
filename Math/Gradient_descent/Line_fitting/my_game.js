66452161fd8e9b8df10dcbf4fd5d83841023511d
Nick <nicholas@hiebl.net.au>
2017-06-07 10:34:13 +1000

    Added exponential fitter

--- Math/Gradient_descent/Line_fitting/my_game.js
+++ Math/Gradient_descent/Line_fitting/my_game.js
@@ -63,11 +63,51 @@ class Parabola {
         }
     }
 }
+class Exponential {
+    constructor() {
+        this.a = 0.1;
+        this.b = 1;
+        this.k = 1;
+        this.c = 0.1;
+        this.learning_rate = 0.05;
+    }
+    guess(x) {
+        return this.a + this.b * Math.exp(this.k * x + this.c);
+    }
+    draw() {
+        stroke(0, 255, 255);
+        var delta = 0.1;
+        for (var x = 0; x <= 1; x += delta) {
+            //console.log('drawing');
+            line(x*width, this.guess(x)*height, (x+delta)*width, this.guess(x+delta)*height);
+        }
+    }
+    gradient_descent(batch) {
+        var da = 0, dk = 0, db = 0, dc = 0;
+        for (var sample of batch) {
+            var expo = this.b * Math.exp(this.k*sample.x + this.c);
+            var guess = this.a + expo;
+            var error = sample.y - guess;
+
+            da += error * this.learning_rate;
+            dk += sample.x * expo * error * this.learning_rate;
+            db += (expo/this.b) * error * this.learning_rate;
+            dc += expo * error * this.learning_rate;
+        }
+        if (batch.length) {
+            this.a += da/batch.length;
+            this.k += dk/batch.length;
+            this.b += db/batch.length;
+            this.c += dc/batch.length;
+        }
+    }
+}
 
 var l = new Line();
 var p = new Parabola();
+var e = new Exponential();
 
-var data = [];
+var data;
 
 function mousePressed(x, y) {
     var d = new Vector(x/width, y/height);
@@ -77,18 +117,25 @@ function mousePressed(x, y) {
 
 function setup() {
     resize(window.innerWidth, window.innerHeight);
+    data = [];
 }
 
 function update() {
     resize(window.innerWidth, window.innerHeight);
-    l.gradient_descent(data);
-    for (var i = 0; i < 10; i ++) p.gradient_descent(data);
+    for (var i = 0; i < 10; i ++) {
+        l.gradient_descent(data);
+        p.gradient_descent(data);
+        for (var j = 0; j < 5; j ++) {
+            e.gradient_descent(data);
+        }
+    }
 }
 
 function draw() {
     background(0);
     l.draw();
     p.draw();
+    e.draw();
     fill(255);
     for (var d of data) {
         ellipse(d.x*width, d.y*height, 3, 3);
