e4d07f3fc115eec92633060d24c96556c33a4f43
nick-hiebl <n.hiebl@student.unsw.edu.au>
2017-12-15 22:13:24 +1100

    Made visual changes to n

--- Nets/my_game.js
+++ Nets/my_game.js
@@ -1,5 +1,9 @@
-function drawCage(x, y) {
-    rect(x, y - 50, 200, 100);
+function drawCage(x, y, state) {
+    //rect(x, y - 10, 20, 20);
+    ellipse(x, y - 10, 10, 10);
+    lineWidth(5);
+    line(x, y - 10, x, y + 50 + 100 * state);
+    rect(x, y + 50 + 100 * state, 200, 100);
 }
 
 var wW, wH;
@@ -46,9 +50,10 @@ function update(dt) {
 function draw() {
     bg(0);
     fill(155);
+    stroke(155);
     if (up) {
         rect(0, 0, 20, 20);
     }
-    drawCage(wW/3, wH/2 + 100 * yState);
-    drawCage(2*wW/3, wH/2 + 100 * (1 - yState));
+    drawCage(wW/3, wH/2, yState);
+    drawCage(2*wW/3, wH/2, 1 - yState);
 }
